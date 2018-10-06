41e2c14273c16478a2a06d312056217cf66d0c5e
DemoliciousDt <league@Thes-Mac-mini.local>
2018-10-06 10:28:45 -0700

    Cat
    Finished the runner

---
+++ src/CatRunner.java
@@ -0,0 +1,16 @@
+
+public class CatRunner {
+public static void main(String[] args) {
+	 Cat rob = new Cat("rob");
+	 rob.meow();
+	 rob.printName();
+	 for(int i= 0; i<=90; i++) {
+	 rob.kill();
+	 }
+}
+}
+		//1. Make the Cat meow
+
+		// 2. Get the Cat to print it's name
+
+		// 3. Kill the Cat!
